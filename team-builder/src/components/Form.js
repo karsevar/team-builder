aa4de2fb25bf42136b34edcdb77e21e4c35b97de
masonkarsevar <masonkarsevar@gmail.com>
2019-07-24 08:26:04 -0700

    Basic test works. WIP: print out output as cards and a state data structure that can be updated

--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -3,34 +3,36 @@ import React from 'react';
 function Form({teamMembers, handleChange, handleSubmit}) {
     return (
         <div className='form-container'>
-            <form onSubmit={event => handleSubmit(event)} />
-            <label>
-                Name: 
-                <input 
-                    type="text"
-                    name="name"
-                    value={teamMembers.name}
-                    onChange={event => handleChange(event)}
-                />
-            </label>
-            <label>
-                Email:
-                <input
-                    type="text"
-                    name="email"
-                    value={teamMembers.email} 
-                    onChange={event => handleChange(event)}
-                />
-            </label>
-            <label>
-                Role:
-                <input
-                    type="text"
-                    name="role"
-                    value={teamMembers.role} 
-                    onChange={event => handleChange(event)}
-                />
-            </label>
+            <form onSubmit={event => handleSubmit(event)}>
+                <label>
+                    Name: 
+                    <input 
+                        type="text"
+                        name="name"
+                        value={teamMembers.name}
+                        onChange={event => handleChange(event)}
+                    />
+                </label>
+                <label>
+                    Email:
+                    <input
+                        type="text"
+                        name="email"
+                        value={teamMembers.email} 
+                        onChange={event => handleChange(event)}
+                    />
+                </label>
+                <label>
+                    Role:
+                    <input
+                        type="text"
+                        name="role"
+                        value={teamMembers.role} 
+                        onChange={event => handleChange(event)}
+                    />
+                </label>
+                <button>Submit New Member!</button>
+            </form>
         </div>
     );
 }
